7046249bf39ceea335232b04b2cc5bf27ffafef9
JDgomez2002 <gom21429@uvg.edu.gt>
2022-03-22 00:43:24 -0600

    Main

--- CalculadoraLisp.java
+++ CalculadoraLisp.java
@@ -1,33 +1,14 @@
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class CalculadoraLisp {
-    public static void main(String[] args) {
-        Vista v = new Vista();
-        String lisp = v.solicitar_lisp();
-
-        Calculadora c = new Calculadora(lisp);
-        // c.separador();
-        String[] expresionLisp = c.get_expresion();
-
-        for(int k = 0; k<expresionLisp.length ;k++){
-            System.out.println(expresionLisp[k]);
-        }
-
-        System.out.println();
-        
-    }
-}
-
-class Calculadora{
-
+    
     private String[] lispArray;
     private int contador;
     private ArrayList<StackLisp<String>> myPrincipal = new ArrayList<StackLisp<String>>();
 
 
-    public Calculadora(String s){
+    public CalculadoraLisp(String s){
         this.lispArray = expresion_reader(s);
         this.contador = 0;
     }
@@ -97,18 +78,5 @@ class Calculadora{
     public String[] get_expresion(){
         return this.lispArray;
     }
-}
 
-class Vista{
-    Scanner scanner = new Scanner(System.in);
-
-    public String solicitar_lisp(){
-        System.out.println();
-        System.out.print("Ingrese su expresion Lisp! R// ");
-        System.out.println();
-        scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
-        return s;
-    }
-    
 }
--- MainG2.java
+++ MainG2.java
@@ -1,13 +1,35 @@
+
+import java.util.Scanner;
+
 public class MainG2{
     public static void main(String[] args) {
         
+        Vista v = new Vista();
+        String lisp = v.solicitar_lisp();
+
+        CalculadoraLisp c = new CalculadoraLisp(lisp);
+        // c.separador();
+        String[] expresionLisp = c.get_expresion();
+
+        for(int k = 0; k<expresionLisp.length ;k++){
+            System.out.println(expresionLisp[k]);
+        }
+
+        System.out.println();
+            
     }
 }
 
-/*///////////////////////////////
-//VISTA
-///////////////////////////////*/
+class Vista{
+    Scanner scanner = new Scanner(System.in);
 
-// class Vista{
+    public String solicitar_lisp(){
+        System.out.println();
+        System.out.print("Ingrese su expresion Lisp! R// ");
+        System.out.println();
+        scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        return s;
+    }
     
-// }
+}
