58c3408e89bc0b330052938c43dc4d59ca476dfc
WalterCruz1600 <72421497+WalterCruz1600@users.noreply.github.com>
2022-03-22 16:57:39 -0600

    Suma simple
    SE ha logrado la suma sencilla

--- MainG2.java
+++ MainG2.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainG2{
@@ -10,14 +11,99 @@ public class MainG2{
         CalculadoraLisp c = new CalculadoraLisp(lisp);
         // c.separador();
         String[] expresionLisp = c.get_expresion();
+        ArrayList<String> expresion= new ArrayList<>();
 
         for(int k = 0; k<expresionLisp.length ;k++){
-            System.out.println(expresionLisp[k]);
+            
+            expresion.add(expresionLisp[k]);
+            //System.out.println(expresion.get(k));
+
         }
 
-        System.out.println();
+        int total=operar(expresion);
+
+        System.out.println("El total es de "+total);
             
     }
+
+    public static int sum(ArrayList<String> ArrayNum){
+
+        int suma=0;
+
+        for(int i=0;i<ArrayNum.size();i++){
+
+            System.out.println(ArrayNum.get(i));
+            suma=suma+Integer.parseInt(ArrayNum.get(i));
+        }
+
+        return suma;
+    }
+
+    public static int operar(ArrayList<String> expresion){
+        int z=0;
+        boolean encontroSuma=false;
+
+        for(int k = 0; k<expresion.size() ;k++){
+
+
+            //System.out.println(expresion.get(k));
+
+            String x=expresion.get(k);
+            System.out.println("x es igual a "+x);
+
+            
+
+            if(x.equals("+")&&encontroSuma==false){
+                encontroSuma=true;
+                System.out.println("si encuentra!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                boolean agregarSuma=false;
+                ArrayList<String> ArraySuma=new ArrayList<>();
+                ArrayList<String> ArrayNum=new ArrayList<>();
+
+                for(int j = k; j<expresion.size() ;j++){
+                    if(agregarSuma==true){
+                        ArraySuma.add(expresion.get(j));
+                    }
+                    if(expresion.get(j).equals("(")){
+                        agregarSuma=true;
+                        ArraySuma.add(expresion.get(j));
+                    }
+                    if(expresion.get(j).equals(")")){
+                        agregarSuma=false;
+                        ArraySuma.add(expresion.get(j));
+                        int t=operar(ArraySuma);
+                        String u=""+t;
+                        ArrayNum.add(u);
+
+                    }
+                    if(expresion.get(j).equals("0")||expresion.get(j).equals("1")||expresion.get(j).equals("2")||expresion.get(j).equals("3")||expresion.get(j).equals("4")||expresion.get(j).equals("5")||expresion.get(j).equals("6")||expresion.get(j).equals("7")||expresion.get(j).equals("8")||expresion.get(j).equals("9")){
+                        
+                        if(!agregarSuma){
+                            ArrayNum.add(expresion.get(j));
+                        }
+                        
+                        
+                    }
+                }
+                
+
+                z= sum(ArrayNum);
+            }
+        }
+
+
+
+        return z;
+
+    }
+
+
+
+
+
+
+    
+
 }
 
 class Vista{
@@ -33,3 +119,7 @@ class Vista{
     }
     
 }
+
+
+
+
